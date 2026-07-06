01bac74de1320e5df66eaeeb2854406f2236ab59
RATNADEEPIKA B <boosaratnadeepika048@gmail.com>
2026-07-06 06:51:37 +0530

    Add files via upload

---
+++ src/NOT_gate.v
@@ -0,0 +1,8 @@
+module NOT_gate(
+    input a,
+    output y
+);
+
+assign y = ~a;
+
+endmodule
---
+++ src/OR_gate.v
@@ -0,0 +1,8 @@
+module or_gate(
+    input a,
+    input b,
+    output y
+);
+
+assign y=a|b;
+endmodule
